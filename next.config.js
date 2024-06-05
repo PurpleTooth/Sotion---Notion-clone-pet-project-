c6a0081e6223a46c4a6043ca298cfec673d0bc80
Rodion <eclipselucky@gmail.com>
2024-06-05 11:07:01 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "files.edgestore.dev"
-    ]
-  }
-}
+    domains: ["files.edgestore.dev"],
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
